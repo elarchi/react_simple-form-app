936ee6e0806e239244ef93c336be654ba5257b68
Emilie Leury <emilieleury@eleury.local>
2022-02-03 18:21:16 +0100

    form terminé

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import "./App.css";
 import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  /* 
+  ===> création de différents 'états' et initialisation de ceux-ci 
+  ===> aka. initialisation des valeurs entrées dans les différents champs du formulaire 
+  ===> les champs du formulaire sont des chaines de caractères vides.   */
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+
   return (
+    // ===> A partir d'ici, nous devons utiliser seulement du JSX
+
     <div className="App">
       <main>
-        <Form />
+        <Form
+          // ===> passons des props à l'enfant de App, soit le composant Form
+
+          // ===> premièrement passons en props les différents états initialisés
+          nom={username}
+          mail={email}
+          mdp={password}
+          confirmationmdp={confirmpassword}
+          // ===> puis passons en props les différents appel de fonction
+          setNom={setUsername}
+          setMail={setEmail}
+          setMdp={setPassword}
+          setConfirmationMdp={setConfirmPassword}
+        />
       </main>
 
-      {/* /* *****************************  footer */}
       <footer>
         <p>
           Made with <span>React</span> at{" "}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,82 @@
-const Form = () => {
+// ===> ici les props reçues par Form sont en destructuring
+const Form = ({
+  nom,
+  mail,
+  mdp,
+  confirmationMdp,
+  setNom,
+  setMail,
+  setMdp,
+  setConfirmationMdp,
+}) => {
+  // ===>créons ensuite la fonction permettant d'envoyer les données entrées dans les différents champs du formulaire (aka. ce qui se passe lors du 'submit'  */
+
+  const handleSubmit = (event) => {
+    // empêcher le rafraîchissement par défaut de la page
+    event.preventDefault();
+    // faire une requête pour envoyer les données du formulaire au serveur...
+
+    // ===> vérifions dans notre console que les informations entrées par l'utilisateur sont bien reçues
+    console.log("nom===> ", nom);
+    console.log("mail ===>", mail);
+    console.log("mdp ===>", mdp);
+    console.log("confirmationMdp ===> ", confirmationMdp);
+  };
+
   return (
-    <div className="form_div">
-      <h1>Create account</h1>
-      <h2>Name</h2>
-      <input type="text" placeholder="Petitejam Ausoussol" />
-      <h2>Email</h2>
-      <input type="email" placeholder="unebatterieausousol@dureacteur.fr" />
-      <h2>Password</h2>
-      <input type="password" placeholder="create your own password here" />
-      <h2>Confirm your password</h2>
-      <input classname="submit_input" type="submit" value={"Register"}></input>
-    </div>
+    <form onSubmit={handleSubmit} className="form_div">
+      <div className="title_div">
+        <h1 className="title_h1">Create account</h1>
+      </div>
+
+      <div className="fields_div">
+        <h2 className="title_h2">Name</h2>
+        <input
+          value={nom}
+          type="text"
+          placeholder="Mme Drums"
+          // la fonction de onChange sera déclencée à chaque modification dans l'input
+          onChange={(event) => {
+            // console.log(event.target.value);
+            setNom(event.target.value);
+          }}
+        />
+        <h2>Email</h2>
+        <input
+          value={mail}
+          type="email"
+          placeholder="unebatterieausousol@dureacteur.fr"
+          onChange={(event) => {
+            // console.log(event.target.value);
+            setMail(event.target.value);
+          }}
+        />
+        <h2>Password</h2>
+        <input
+          value={mdp}
+          type="password"
+          placeholder="create your own password here"
+          onChange={(event) => {
+            // console.log(event.target.value);
+            setMdp(event.target.value);
+          }}
+        />
+        <h2>Confirm your password</h2>
+        <input
+          value={confirmationMdp}
+          type="password"
+          placeholder="confirm your password"
+          onChange={(event) => {
+            // console.log(event.target.value);
+            setConfirmationMdp(event.target.value);
+          }}
+        />
+      </div>
+
+      <button value={confirmationMdp} classname="submit_input" type="submit">
+        Register
+      </button>
+    </form>
   );
 };
 export default Form;
